55575468e805b015103a2c8321fb070d315efbe2
27medwa2el <a7med242@gmail.com>
2024-07-15 11:31:20 +0300

    adding changes

--- src/app/components/home/market-summary/market-summary.component.ts
+++ src/app/components/home/market-summary/market-summary.component.ts
@@ -7,20 +7,23 @@ interface Market {
   changePercentage: number;
 }
 
+interface SummaryData {
+  topGainers: number;
+  topLosers: number;
+  stockCount: number;
+  tradesCount: number;
+  totalVolume: number;
+  totalValue: number;
+  lastUpdated: string;
+}
+
 @Component({
   selector: 'app-market-summary',
   templateUrl: './market-summary.component.html',
   styleUrls: ['./market-summary.component.scss']
 })
 export class MarketSummaryComponent implements OnInit {
-  topGainers: number = 35;
-  topLosers: number = 53;
-  stockCount: number = 202;
-  tradesCount: number = 25644;
-  totalVolume: number = 184313.642;
-  totalValue: number = 742596.464;
-  lastUpdated: string = '15/05/2024 11:14:37';
-
+  summaryData: SummaryData;
   markets: Market[] = [
     { name: 'Abu Dhabi', marketValue: 9093.27, changeValue: 0.76, changePercentage: 0.1 },
     { name: 'Jordan', marketValue: 4662.43, changeValue: -45.47, changePercentage: -0.9 },
@@ -30,7 +33,55 @@ export class MarketSummaryComponent implements OnInit {
     { name: 'Qatar', marketValue: 4900.33, changeValue: 5.58, changePercentage: 0.12 },
   ];
 
-  constructor() {}
+  marketSummary: SummaryData = {
+    topGainers: 35,
+    topLosers: 53,
+    stockCount: 202,
+    tradesCount: 25644,
+    totalVolume: 184313.642,
+    totalValue: 742596.464,
+    lastUpdated: '15/05/2024 11:14:37'
+  };
+
+  egxSummary: SummaryData = {
+    topGainers: 40,
+    topLosers: 30,
+    stockCount: 180,
+    tradesCount: 20000,
+    totalVolume: 150000,
+    totalValue: 600000,
+    lastUpdated: '15/05/2024 10:00:00'
+  };
+
+  tradingSummary: SummaryData = {
+    topGainers: 50,
+    topLosers: 45,
+    stockCount: 220,
+    tradesCount: 30000,
+    totalVolume: 200000,
+    totalValue: 800000,
+    lastUpdated: '15/05/2024 09:00:00'
+  };
+
+  constructor() {
+    this.summaryData = this.marketSummary;
+  }
 
   ngOnInit(): void {}
+
+  changeSummary(type: string): void {
+    switch (type) {
+      case 'market':
+        this.summaryData = this.marketSummary;
+        break;
+      case 'egx':
+        this.summaryData = this.egxSummary;
+        break;
+      case 'trading':
+        this.summaryData = this.tradingSummary;
+        break;
+      default:
+        this.summaryData = this.marketSummary;
+    }
+  }
 }
